f918e83db1783861e08564c27d88705c97287cbe
gongsidiannao <756565328@qq.com>
2023-10-30 10:41:18 +0800

    1

--- src/main/java/com/Test/controller.java
+++ src/main/java/com/Test/controller.java
@@ -1,4 +1,30 @@
 package com.Test;
 
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class controller {
+
+    public static void main(String[] args) {
+        ArrayList<String> strings = new ArrayList<>();
+        strings.add("1");
+        strings.add("2");
+        strings.add("3");
+        strings.add("4");
+        strings.add("5");
+        strings.add("2");
+        Iterator<String> iterator = strings.iterator();
+
+        while (iterator.hasNext()){
+            String next = iterator.next();
+            if ("2".equals(next)){
+                iterator.remove();
+            }
+        }
+
+        System.out.println(strings);
+    }
+
 }
+  
